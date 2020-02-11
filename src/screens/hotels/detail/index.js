d98d17c93a91cf415bb69830a9b8e02deb52b7da
Cristian Hernandez <ing.cristianh@gmail.com>
2020-02-11 17:10:24 -0500

    View detail hotel

--- src/screens/hotels/detail/index.js
+++ src/screens/hotels/detail/index.js
@@ -1,38 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import {getHotelDetails} from '../../../api';
+import HotelCardDetail from './components/Card';
 
-const HotelDetail: () => React = props => {
-  const [details, setDetails] = React.useState();
-
-  const {_id, name, price} = props.route.params.hotel;
-  let latitude;
-  let longitude;
-  let address;
-  let comments = [];
-  let images = [];
-
-  React.useEffect(() => {
-    getDetailHotel();
-  }, []);
-
-  const getDetailHotel = async () => {
-    const data = await getHotelDetails(_id);
-    setDetails(data);
-  };
-
-  if (details) {
-    latitude = details.latitude;
-    longitude = details.longitude;
-    address = details.address;
-  }
+const HotelDetail: () => React<Props> = props => {
+  const {hotel} = props.route.params;
 
   return (
-    <View>
-      <Text>{name}</Text>
-      <Text>{latitude}</Text>
-      <Text>{longitude}</Text>
-      <Text>{address}</Text>
+    <View style={{flex: 1}}>
+      <HotelCardDetail hotel={hotel}></HotelCardDetail>
     </View>
   );
 };
