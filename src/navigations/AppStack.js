712ea10b4b56c9b69b874f33f2d31d592bde768e
Cristian Hernandez <ing.cristianh@gmail.com>
2020-02-11 12:19:42 -0500

    Add stack HotelDetail and title route dynamic, add title to home

--- src/navigations/AppStack.js
+++ src/navigations/AppStack.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import Hotels from '../views/hotels';
+import Hotels from '../screens/hotels/home';
+import HotelDetail from '../screens/hotels/detail';
 
 const Stack = createStackNavigator();
 
 const AppStack: () => React = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Hotels" component={Hotels} />
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={Hotels}
+        options={{title: 'Listado de hoteles'}}
+      />
+      <Stack.Screen
+        name="HotelDetail"
+        component={HotelDetail}
+        options={({route}) => ({title: route.params.hotel.name})}
+      />
     </Stack.Navigator>
   );
 };
